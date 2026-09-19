5d5c1f49ac5492946519a293ec50ddb72b5c053c
Renan Vitor <renanbri99@gmail.com>
2026-09-19 02:59:58 -0400

    mudança no layout da calculadora

--- lib/main.dart
+++ lib/main.dart
@@ -1,332 +1,244 @@
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() {
-  runApp(const CalculadoraApp());
+  runApp(MyApp());
 }
 
-class CalculadoraApp extends StatelessWidget {
-  const CalculadoraApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Calculadora',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        useMaterial3: true,
-      ),
-      home: const CalculadoraPage(),
-    );
+      home: HomePage(),
+    ); // MaterialApp
   }
 }
 
-class CalculadoraPage extends StatefulWidget {
-  const CalculadoraPage({super.key});
-
+class HomePage extends StatefulWidget {
   @override
-  State<CalculadoraPage> createState() => _CalculadoraPageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _CalculadoraPageState extends State<CalculadoraPage> {
-  String display = '0';
+class _HomePageState extends State<HomePage> {
+  var userInput = '';
+  var answer = '';
 
-  double? primeiroNumero;
-  String? operador;
-  bool novoNumero = false;
-
-  void pressionarNumero(String numero) {
-    setState(() {
-      if (display == '0' || novoNumero) {
-        display = numero;
-        novoNumero = false;
-      } else {
-        display += numero;
-      }
-    });
-  }
-
-  void pressionarDecimal() {
-    setState(() {
-      if (novoNumero) {
-        display = '0.';
-        novoNumero = false;
-      } else if (!display.contains('.')) {
-        display += '.';
-      }
-    });
-  }
-
-  void pressionarOperador(String novoOperador) {
-    final numeroAtual = double.tryParse(display);
-
-    if (numeroAtual == null) return;
-
-    setState(() {
-      if (primeiroNumero != null && operador != null) {
-        calcular();
-      } else {
-        primeiroNumero = numeroAtual;
-      }
-
-      operador = novoOperador;
-      novoNumero = true;
-    });
-  }
-
-  void calcular() {
-    if (primeiroNumero == null || operador == null) return;
-
-    final segundoNumero = double.tryParse(display);
-
-    if (segundoNumero == null) return;
-
-    double resultado;
-
-    switch (operador) {
-      case '+':
-        resultado = primeiroNumero! + segundoNumero;
-        break;
-
-      case '-':
-        resultado = primeiroNumero! - segundoNumero;
-        break;
-
-      case '×':
-        resultado = primeiroNumero! * segundoNumero;
-        break;
-
-      case '÷':
-        if (segundoNumero == 0) {
-          display = 'Erro';
-          primeiroNumero = null;
-          operador = null;
-          novoNumero = true;
-          return;
-        }
-
-        resultado = primeiroNumero! / segundoNumero;
-        break;
-
-      default:
-        return;
-    }
-
-    display = formatarNumero(resultado);
-    primeiroNumero = resultado;
-    operador = null;
-  }
-
-  String formatarNumero(double numero) {
-    if (numero == numero.toInt()) {
-      return numero.toInt().toString();
-    }
-
-    return numero.toString();
-  }
-
-  void pressionarIgual() {
-    setState(() {
-      calcular();
-      novoNumero = true;
-    });
-  }
-
-  void limpar() {
-    setState(() {
-      display = '0';
-      primeiroNumero = null;
-      operador = null;
-      novoNumero = false;
-    });
-  }
-
-  void apagar() {
-    setState(() {
-      if (display.length > 1) {
-        display = display.substring(0, display.length - 1);
-      } else {
-        display = '0';
-      }
-    });
-  }
-
-  Widget botao(
-    String texto, {
-    VoidCallback? onPressed,
-    bool operador = false,
-    bool igual = false,
-  }) {
-    return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.all(5),
-        child: ElevatedButton(
-          onPressed: onPressed,
-          style: ElevatedButton.styleFrom(
-            backgroundColor: igual
-                ? Colors.orange
-                : operador
-                    ? Colors.blueGrey.shade700
-                    : Colors.grey.shade900,
-            foregroundColor: Colors.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(18),
-            ),
-          ),
-          child: Text(
-            texto,
-            style: const TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+  // Array of button
+  final List<String> buttons = [
+    'C',
+    '+/-',
+    '%',
+    'DEL',
+    '7',
+    '8',
+    '9',
+    '/',
+    '4',
+    '5',
+    '6',
+    'x',
+    '1',
+    '2',
+    '3',
+    '-',
+    '0',
+    '.',
+    '=',
+    '+',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: const Text('Calculadora'),
-        backgroundColor: Colors.black,
-      ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            // Display
-            Expanded(
-              flex: 2,
-              child: Container(
-                alignment: Alignment.bottomRight,
-                padding: const EdgeInsets.all(25),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Text(
-                    display,
-                    style: const TextStyle(
-                      fontSize: 52,
-                      fontWeight: FontWeight.w300,
+        title: Text("Calculator"),
+        backgroundColor: Colors.blueAccent,
+        foregroundColor: Colors.white,
+      ), //AppBar
+      backgroundColor: Colors.white38,
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        userInput,
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
                     ),
-                  ),
+                    Container(
+                      padding: EdgeInsets.all(15),
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        answer,
+                        style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  ]),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              child: GridView.builder(
+                  itemCount: buttons.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4),
+                  itemBuilder: (BuildContext context, int index) {
+                    // Clear Button
+                    if (index == 0) {
+                      return MyButton(
+                        buttontapped: () {
+                          setState(() {
+                            userInput = '';
+                            answer = '0';
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: Colors.blue[50],
+                        textColor: Colors.black,
+                      );
+                    }
+
+                    // +/- button
+                    else if (index == 1) {
+                      return MyButton(
+                        buttonText: buttons[index],
+                        color: Colors.blue[50],
+                        textColor: Colors.black,
+                      );
+                    }
+                    // % Button
+                    else if (index == 2) {
+                      return MyButton(
+                        buttontapped: () {
+                          setState(() {
+                            userInput += buttons[index];
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: Colors.blue[50],
+                        textColor: Colors.black,
+                      );
+                    }
+                    // Delete Button
+                    else if (index == 3) {
+                      return MyButton(
+                        buttontapped: () {
+                          setState(() {
+                            userInput =
+                                userInput.substring(0, userInput.length - 1);
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: Colors.blue[50],
+                        textColor: Colors.black,
+                      );
+                    }
+                    // Equal_to Button
+                    else if (index == 18) {
+                      return MyButton(
+                        buttontapped: () {
+                          setState(() {
+                            equalPressed();
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: Colors.orange[700],
+                        textColor: Colors.white,
+                      );
+                    }
+
+                    //  other buttons
+                    else {
+                      return MyButton(
+                        buttontapped: () {
+                          setState(() {
+                            userInput += buttons[index];
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: isOperator(buttons[index])
+                            ? Colors.blueAccent
+                            : Colors.white,
+                        textColor: isOperator(buttons[index])
+                            ? Colors.white
+                            : Colors.black,
+                      );
+                    }
+                  }), // GridView.builder
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  bool isOperator(String x) {
+    if (x == '/' || x == 'x' || x == '-' || x == '+' || x == '=') {
+      return true;
+    }
+    return false;
+  }
+
+// function to calculate the input operation
+  void equalPressed() {
+    String finaluserinput = userInput;
+    finaluserinput = userInput.replaceAll('x', '*');
+
+    Parser p = Parser();
+    Expression exp = p.parse(finaluserinput);
+    ContextModel cm = ContextModel();
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+    answer = eval.toString();
+  }
+}
+
+// creating Stateless Widget for buttons
+class MyButton extends StatelessWidget {
+  // declaring variables
+  final color;
+  final textColor;
+  final String buttonText;
+  final buttontapped;
+
+  //Constructor
+  MyButton(
+      {this.color,
+      this.textColor,
+      required this.buttonText,
+      this.buttontapped});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: buttontapped,
+      child: Padding(
+        padding: const EdgeInsets.all(0.2),
+        child: ClipRRect(
+          // borderRadius: BorderRadius.circular(25),
+          child: Container(
+            color: color,
+            child: Center(
+              child: Text(
+                buttonText,
+                style: TextStyle(
+                  color: textColor,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-
-            // Botões
-            Expanded(
-              flex: 5,
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      botao(
-                        'C',
-                        onPressed: limpar,
-                        operador: true,
-                      ),
-                      botao(
-                        '⌫',
-                        onPressed: apagar,
-                        operador: true,
-                      ),
-                      botao(
-                        '÷',
-                        onPressed: () => pressionarOperador('÷'),
-                        operador: true,
-                      ),
-                    ],
-                  ),
-
-                  Row(
-                    children: [
-                      botao(
-                        '7',
-                        onPressed: () => pressionarNumero('7'),
-                      ),
-                      botao(
-                        '8',
-                        onPressed: () => pressionarNumero('8'),
-                      ),
-                      botao(
-                        '9',
-                        onPressed: () => pressionarNumero('9'),
-                      ),
-                      botao(
-                        '×',
-                        onPressed: () => pressionarOperador('×'),
-                        operador: true,
-                      ),
-                    ],
-                  ),
-
-                  Row(
-                    children: [
-                      botao(
-                        '4',
-                        onPressed: () => pressionarNumero('4'),
-                      ),
-                      botao(
-                        '5',
-                        onPressed: () => pressionarNumero('5'),
-                      ),
-                      botao(
-                        '6',
-                        onPressed: () => pressionarNumero('6'),
-                      ),
-                      botao(
-                        '-',
-                        onPressed: () => pressionarOperador('-'),
-                        operador: true,
-                      ),
-                    ],
-                  ),
-
-                  Row(
-                    children: [
-                      botao(
-                        '1',
-                        onPressed: () => pressionarNumero('1'),
-                      ),
-                      botao(
-                        '2',
-                        onPressed: () => pressionarNumero('2'),
-                      ),
-                      botao(
-                        '3',
-                        onPressed: () => pressionarNumero('3'),
-                      ),
-                      botao(
-                        '+',
-                        onPressed: () => pressionarOperador('+'),
-                        operador: true,
-                      ),
-                    ],
-                  ),
-
-                  Row(
-                    children: [
-                      botao(
-                        '0',
-                        onPressed: () => pressionarNumero('0'),
-                      ),
-                      botao(
-                        '.',
-                        onPressed: pressionarDecimal,
-                      ),
-                      botao(
-                        '=',
-                        onPressed: pressionarIgual,
-                        igual: true,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
